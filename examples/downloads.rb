2b62ea7dec5feb6d180ba994f1313cbafbdf82fd
SKAhack <m@skahack.com>
2011-09-05 13:14:55 +0900

    Update example

--- examples/downloads.rb
+++ examples/downloads.rb
@@ -3,16 +3,10 @@ require './common'
 m = login
 
 uuid = create_uuid
-folder1 = m.create_folder(uuid, root_folder)
-
-uuid = create_uuid
-folder2 = m.create_folder(uuid, root_folder)
-
-uuid = create_uuid
 File.open(uuid, "w") do |f|
   f.write(uuid)
 end
-file1 = m.upload(uuid, folder1)
+file1 = m.upload(uuid)
 File.delete(uuid)
 puts 'upload file1'
 
@@ -20,11 +14,11 @@ uuid = create_uuid
 File.open(uuid, "w") do |f|
   f.write(uuid)
 end
-file2 = m.upload(uuid, folder2)
+file2 = m.upload(uuid)
 File.delete(uuid)
 puts 'upload file2'
 
 files = [file1, file2]
 
 
-m.downloads(files, nil)
+m.downloads(files)
